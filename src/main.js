63e3e9f0bdbfcb0e4b333f831fa13f178706642e
Mary1-com <vnnkvmrn76@gmail.com>
2026-03-17 14:55:12 +0200

    First commit 11 HW

---
+++ src/main.js
@@ -0,0 +1,51 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from "./js/pixabay-api.js";
+
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from "./js/render-functions.js";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const query = e.target.elements["search-text"].value.trim();
+
+    if (!query) {
+        iziToast.error({
+            message: "Please enter a search query!",
+        });
+        return;
+    }
+
+    clearGallery();
+    showLoader();
+
+    try {
+        const data = await getImagesByQuery(query);
+
+        if (data.hits.lenght === 0) {
+            iziToast.error({
+                message: "Sorry, there are no images matching your search query. Please try again!",
+            })
+            return;
+        }
+
+        createGallery(data.hits);
+
+    } catch (error) {
+        iziToast.error({
+            message: "Something went wrong. Try again!",
+        });
+
+    } finally {
+        hideLoader();
+    }
+});
